a592438cac662cc017515c09bf916d1f0cf05db3
Gaurav <gauravpawar9921@gmail.com>
2023-10-12 12:59:17 +0530

    Removed Sysout

--- src/DbProg.java
+++ src/DbProg.java
@@ -36,11 +36,6 @@ public class DbProg {
 	         
 	         
 	         while (rs.next()) {
-
-	        	 System.out.println("Swimming Pool Local Database feature1 5432");
-
-	        	 System.out.println("Swimming Pool Local Database 5433");
-
 	             System.out.print("Total Count : " + rs.getLong("Total"));
 	         }
 	      } catch (SQLException e) {
